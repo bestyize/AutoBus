81c246cfb2f81d15422d28b8bbc4fd84631fe0aa
yize <3011429848@qq.com>
2020-05-16 20:50:24 +0800

    采用异步方式处理数据发布

--- autobus/src/main/java/com/yize/autobus/MainPublisher.java
+++ autobus/src/main/java/com/yize/autobus/MainPublisher.java
@@ -8,6 +8,8 @@ import androidx.annotation.NonNull;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.LinkedList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MainPublisher extends Handler implements Publisher {
     private static final int MAIN_DATA=0;
@@ -22,21 +24,53 @@ public class MainPublisher extends Handler implements Publisher {
         }
     }
     private LinkedList<InnerPublishData> publishQueue;
+    private Object LOCK=new Object();
+    private Thread publishThread;
 
     public MainPublisher() {
         super(Looper.getMainLooper());
         this.publishQueue = new LinkedList<InnerPublishData>();
     }
 
+    /**
+     * 订阅入队，交给一个线程单独处理。
+     * 为了防止很多消息同时发送的情况，需要采用异步的方式，而且需要保证线程安全
+     * @param subscription
+     * @param data
+     */
     public void enqueue(Subscription subscription, Object data) {
         InnerPublishData publishData=new InnerPublishData(subscription,data);
-        publishQueue.offer(publishData);
+        synchronized (LOCK){
+            publishQueue.offer(publishData);
+            LOCK.notifyAll();
+        }
+        if(publishThread==null){
+            publishThread=new Thread(new InnerPublishRunnable());
+            publishThread.start();
+        }
+    }
 
-        while (!publishQueue.isEmpty()){
-            Message message=new Message();
-            message.what=MAIN_DATA;
-            message.obj=publishQueue.pollLast();
-            sendMessage(message);
+    private class InnerPublishRunnable implements Runnable{
+
+        @Override
+        public void run() {
+            while (true){
+                synchronized (LOCK){
+                    while (publishQueue.isEmpty()){
+                        try {
+                            LOCK.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    while (!publishQueue.isEmpty()){
+                        Message message=new Message();
+                        message.what=MAIN_DATA;
+                        message.obj=publishQueue.pollLast();
+                        sendMessage(message);
+                    }
+                }
+            }
         }
     }
 
